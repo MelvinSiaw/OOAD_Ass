dff0e1dc13bcc36257dfa55b25a485d206c88052
chongjiensheng <134246439+chongjiensheng@users.noreply.github.com>
2025-07-05 20:10:27 +0800

    done FeeCalculator

--- model/Bill.java
+++ model/Bill.java
@@ -1,23 +1,50 @@
 package model;
 
 public class Bill {
-    private final double baseFee;
-    private final double extraServices;
-    private final double discountAmount;
-    private final double total;
+    private final double baseFee;              
+    private final double extraServices;        
+    private final double totalBeforeDiscount;  
+    private final double discountAmount;       
+    private final double netPayable;          
 
-    public Bill(double baseFee, double extraServices, double discountAmount, double total) {
+    public Bill(double baseFee, double extraServices, double discountAmount) {
         this.baseFee = baseFee;
         this.extraServices = extraServices;
+        this.totalBeforeDiscount = baseFee + extraServices;
         this.discountAmount = discountAmount;
-        this.total = total;
+        this.netPayable = totalBeforeDiscount - discountAmount;
+    }
+
+    public double getBaseFee() {
+        return baseFee;
+    }
+
+    public double getExtraServices() {
+        return extraServices;
+    }
+
+    public double getTotalBeforeDiscount() {
+        return totalBeforeDiscount;
+    }
+
+    public double getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public double getNetPayable() {
+        return netPayable;
     }
 
     @Override
     public String toString() {
-        return String.format(
-            "Base Fee: RM%.2f\nExtra Services: RM%.2f\nDiscount: RM%.2f\nTotal Payable: RM%.2f",
-            baseFee, extraServices, discountAmount, total
-        );
+        return String.format("""
+    Base Fee:              RM%.2f
+    Extra Services:        RM%.2f
+    Total Before Discount: RM%.2f
+    Discount:              RM%.2f
+    Net Payable:           RM%.2f
+    """,
+    baseFee, extraServices, totalBeforeDiscount, discountAmount, netPayable
+);
     }
 }
--- model/FeeCalculator.java
+++ model/FeeCalculator.java
@@ -1,21 +1,28 @@
 package model;
 
 public class FeeCalculator {
+
     public static Bill calculate(Registration reg, DiscountStrategy discountStrategy) {
-    double base = reg.getEvent().getFee();
-    double extra = 0;
-    if (reg.hasCatering()) extra += 20;
-    if (reg.hasTransportation()) extra += 10;
+        double baseFee = reg.getEvent().getFee();
+        double extraFee = 0;
 
-    double totalBeforeDiscount = (base + extra) * reg.getGroupSize();
-    double discountedTotal = discountStrategy.applyDiscount(totalBeforeDiscount, reg.getGroupSize());
-    double discountAmount = totalBeforeDiscount - discountedTotal;
+        if (reg.hasCatering()) {
+            extraFee += 20;
+        }
+        if (reg.hasTransportation()) {
+            extraFee += 10;
+        }
 
-    return new Bill(
-        base * reg.getGroupSize(),
-        extra * reg.getGroupSize(),
-        discountAmount,
-        discountedTotal
+        int groupSize = reg.getGroupSize();
+
+        double totalBeforeDiscount = (baseFee + extraFee) * groupSize;
+        double discountedTotal = discountStrategy.applyDiscount(totalBeforeDiscount, groupSize);
+        double discountAmount = totalBeforeDiscount - discountedTotal;
+
+        return new Bill(
+        baseFee * groupSize,
+        extraFee * groupSize,
+        discountAmount
     );
-}
+    }
 }
